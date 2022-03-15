f20fea32a00092d24852e32d20eea7d65d1786ae
ZHAOQI CHEN <z.chen5.21@abdn.ac.uk>
2022-03-15 19:14:33 +0000

    The second table commit

--- bears/views.py
+++ bears/views.py
@@ -9,4 +9,8 @@ def bear_detail(request, id):
     bear = get_object_or_404(Bear, id=id)
     return render(request, 'bear_detail.html', {'bear' : bear})
 
+def bear_status(request, id):
+    bears_status = Sighting.objects.all()
+    return render(request, 'bear_status.html', {'bears_status' : bears_status})
+
 # Create your views here.
