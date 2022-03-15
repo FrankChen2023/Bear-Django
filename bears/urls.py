401979c8bbeb760df00a1ffe2709095ac07f646c
ZHAOQI CHEN <z.chen5.21@abdn.ac.uk>
2022-03-15 19:29:01 +0000

    The second table commit

--- bears/urls.py
+++ bears/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.bear_list, name='bear_list'),
-    path('bear/<int:id>/', views.bear_detail, name= 'bear_detail'),
     path('bear/<int:id>/', views.bear_status, name= 'bear_status'),
 ]
